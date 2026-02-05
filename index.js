42316481c37c72e6a4d7c0bb43e0230b75025833
mugisha thicien <mugishathicien04@gmail.com>
2026-02-05 20:09:36 +0200

    Solving the issue with files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var text = "Hello, Thicien!";
+var text = "Hello, Thicien! I am a student at RP Kigali University.";
 var heading = document.createElement("h1");
 heading.textContent = text;
 document.body.appendChild(heading);
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-let text: string = "Hello, Thicien!";
+let text: string = "Hello, Thicien! I am a student at RP Kigali University.";
 let heading = document.createElement("h1");
 heading.textContent = text;
 document.body.appendChild(heading);
