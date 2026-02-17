18ced30a1f35318f050b7c4e70a41a171e218981
mugisha thicien <mugishathicien04@gmail.com>
2026-02-17 16:54:46 +0200

    Working on The most digits katas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var text = "Hello, Thicien! I am a student at RP Kigali University.";
 var heading = document.createElement("h1");
 heading.textContent = text;
+
 document.body.appendChild(heading);
