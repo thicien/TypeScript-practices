21a5b0eaef7327608a8fb3802fa8108c34d846d6
mugisha thicien <mugishathicien04@gmail.com>
2026-02-23 16:38:31 +0200

    .

--- inheritance.ts
+++ inheritance.ts
@@ -10,16 +10,13 @@ class Employee {
     this.salary = salary;
     this.employeeId = Math.floor(Math.random() * 10000);
     Employee.totalEmployees++;
-    }
-    
+    } 
   getDetails(): string {
     return `Name: ${this.name}, Salary: ${this.salary}`;
-    }
-    
+    } 
   getRole(): string {
     return "Employee";
   }
-
   static getTotalEmployees(): number {
     return Employee.totalEmployees;
   }
@@ -29,11 +26,9 @@ class Manager extends Employee {
   constructor(name: string, salary: number, public department: string) {
     super(name, salary);
   }
-
   override getRole(): string {
     return "Manager";
   }
-
   getDepartmentInfo(): string {
     return `${this.name} manages the ${this.department} department`;
   }
@@ -43,26 +38,20 @@ class Developer extends Employee {
   constructor(name: string, salary: number, public programmingLanguage: string) {
     super(name, salary);
   }
-
   override getRole(): string {
     return "Developer";
   }
-
   getTechStack(): string {
     return `${this.name} works with ${this.programmingLanguage}`;
   }
 }
-
 const emp1 = new Manager("Thicien", 5000, "IT");
 const emp2 = new Developer("Alice", 4000, "TypeScript");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
-
 console.log(emp1.getRole());
 console.log(emp2.getRole());
-
 console.log(emp1.getDepartmentInfo());
 console.log(emp2.getTechStack());
-
 console.log(Employee.getTotalEmployees());
