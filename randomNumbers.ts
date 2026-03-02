00b71a1dd8bf177f475fb73cc5175867a8ef13b0
mugisha thicien <mugishathicien04@gmail.com>
2026-03-02 15:26:17 +0200

    Working on the generic types in typescript

--- randomNumbers.ts
+++ randomNumbers.ts
@@ -1,8 +1,11 @@
-function getRandomNumberElement(items: any[]): any {
+function getRandomNumberElement<T>(items: T[]): T {
     let randomIndex = Math.floor(Math.random() * items.length);
     return items[randomIndex];
 }
 let colors = ['orange', 'green', 'black', 'yellow', 'red'];
 let numbers = [1, 3, 4, 2, 6, 7, 8, 9];
-console.log(getRandomNumberElement(colors));
-console.log(getRandomNumberElement(numbers));
+let randomColors = getRandomNumberElement(colors);
+let randomNum = getRandomNumberElement(numbers);
+
+console.log(randomNum);
+console.log(randomColors);
