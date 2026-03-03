00b2d1e17cb364978af64167fc3d9a2bc477255c
mugisha thicien <mugishathicien04@gmail.com>
2026-03-03 13:05:42 +0200

    Completing the logic of getting the the multiples of 3 and 5

--- mutliples.js
+++ mutliples.js
@@ -1,7 +1,6 @@
 export class Challenge {
   static solution(num: number) {
     if (num <= 0) return 0;
-
     let sum = 0;
     for (let i = 0; i < num; i++) {
       if (i % 3 === 0 || i % 5 === 0) {
