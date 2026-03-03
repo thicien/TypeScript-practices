b676af32f6b28b5e66ed3a3975792d2ff9127a54
mugisha thicien <mugishathicien04@gmail.com>
2026-03-03 13:02:40 +0200

    working on the multiples of 3 and 5

---
+++ mutliples.js
@@ -0,0 +1,17 @@
+export class Challenge {
+  static solution(num: number) {
+    if (num <= 0) return 0;
+
+    let sum = 0;
+
+    for (let i = 0; i < num; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
+console.log(Challenge.solution(10));
+console.log(Challenge.solution(20));
+console.log(Challenge.solution(-5));
