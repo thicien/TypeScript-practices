03395c862a23972ac51343288c6a8f10aaa8e4c7
mugisha thicien <mugishathicien04@gmail.com>
2026-03-11 12:49:19 +0200

    The logic for checking that in string there is not other characters

---
+++ notVerySecure.ts
@@ -0,0 +1,15 @@
+export function alphanumeric(string: string): boolean {
+    if (string.length === 0) return false;
+
+    for (let char of string) {
+        if (!/[a - zA-Z0-9]/.test(char)) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
