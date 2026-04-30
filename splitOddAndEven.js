81802bfa3651f398fad6ca23a8a86e7224026150
mugisha thicien <mugishathicien04@gmail.com>
2026-04-30 12:24:04 +0300

    Spliting even and odd numbers in the array

---
+++ splitOddAndEven.js
@@ -0,0 +1,18 @@
+function splitOddAndEven(n) {
+  const digits = String(n);
+  const result = [];
+  let current = digits[0];
+  for (let i = 1; i < digits.length; i++) {
+    let previous = digits[i - 1] % 2;
+    let curry = digits[i] % 2;
+
+    if (previous === curry) {
+      current += digits[i];
+    } else {
+      result.push(Number(current));
+      current = digits[i];
+    }
+  }
+  result.push(Number(current));
+  return result;
+}
