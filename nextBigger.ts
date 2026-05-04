7f875a6eff5dfae674ddfaba0f52732e84b2a661
mugisha thicien <mugishathicien04@gmail.com>
2026-05-04 09:35:22 +0300

    Solging the kata of finding the next biggest integer

--- nextBigger.ts
+++ nextBigger.ts
@@ -4,13 +4,11 @@ export function nextBigger(n: number): number {
   while (i >= 0 && digits[i] >= digits[i + 1]) {
     i--;
   }
-
   if (i < 0) return -1;
   let j = digits.length - 1;
   while (digits[j] <= digits[i]) {
     j--;
   }
-
   [digits[i], digits[j]] = [digits[j], digits[i]];
   const left = digits.slice(0, i + 1);
   const right = digits.slice(i + 1).sort((a, b) => a - b);
