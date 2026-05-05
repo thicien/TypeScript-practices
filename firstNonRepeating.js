75933c1f732e0e030464dc3ee082332631197630
mugisha thicien <mugishathicien04@gmail.com>
2026-05-05 11:34:50 +0300

    Returning the first repeating letters

---
+++ firstNonRepeating.js
@@ -0,0 +1,11 @@
+function firstDup(s) {
+  for (let i = 0; i < s.length; i++) {
+    if (s.indexOf(s[i], i + 1) !== -1) {
+      return s[i];
+    }
+  }
+  return undefined;
+}
+firstDup('tweet');
+firstDup('like');
+firstDup('translator');
